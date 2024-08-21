fecea7b04d27ebe9804f16476cc9c79cb68e7b29
Anshul1996-lang <anshul.soam@facillima.com>
2024-08-21 17:13:40 +0000

    conversionofdatatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3) // we are using node.js not browser
+
+let name = "Anshul"
+let age = 27
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//bigint
+// string=> ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbole => unique
+
+// object
+
+console.log(typeof "Anshul");
+
+
+console.log(typeof null);
